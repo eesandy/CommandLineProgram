f8ad8df2196bba15e19626e4a4010f59208ae207
eesandy <102400532+eesandy@users.noreply.github.com>
2023-03-02 09:49:50 +0530

    # new_file_added@TASKID =1001, 1002 #

---
+++ CommandLineProgram.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class CommandLineProgram {
+
+	public static void main(String[] args) {
+		System.out.println("This is a simple command line program!");
+	    System.out.println("Your Command Line arguments are:"); // loop through all arguments and print it to the user 
+	    
+	    for (String str: args) {
+	        System.out.println(str);
+	     }
+	}
+
+}
